73765f0fea275e9e0a25269a332dda39c23541fe
yxd_lzc <876231865@qq.com>
2024-11-01 15:07:43 +0800

    fix spm

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ZCJSON"),
+            name: "ZCJSON",
+            dependencies: ["ZCMacro"]),
         .testTarget(
             name: "ZCJSONTests",
             dependencies: ["ZCJSON"]),
