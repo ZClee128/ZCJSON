8c136b931ec6355a930ec12b1e4dd8684e6e4fd2
yxd_lzc <876231865@qq.com>
2025-04-25 10:00:53 +0800

    更新spm 资源配置

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ZCJSON",
+    defaultLocalization: "en",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -22,8 +23,8 @@ let package = Package(
         .target(
             name: "ZCJSON",
             dependencies: ["ZCMacro"],
-            path: "ZCJSON",
-            sources: ["Classes"],
+            path: ".",
+            sources: ["ZCJSON/Classes"],
             swiftSettings: [
                 .enableExperimentalFeature("Macros")
             ]
